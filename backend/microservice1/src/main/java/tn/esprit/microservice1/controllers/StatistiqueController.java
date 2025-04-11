430da1aa8ec64fbf1bf6bf5486da5c4b62ceaf2b
TahaNaat <naat.taha@esprit.tn>
2025-04-11 22:32:37 +0100

    fixed gateaway conflict

--- backend/microservice1/src/main/java/tn/esprit/microservice1/controllers/StatistiqueController.java
+++ backend/microservice1/src/main/java/tn/esprit/microservice1/controllers/StatistiqueController.java
@@ -11,7 +11,7 @@ import java.util.Map;
 import java.util.Set;
 
 @RestController
-@RequestMapping("/api/statistiques")
+@RequestMapping("/mic1/statistiques")
 public class StatistiqueController {
 
     @Autowired
