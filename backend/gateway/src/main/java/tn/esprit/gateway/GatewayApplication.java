1eaa5beca2a79f3c74796843812ea77928f1f720
iitsMahdi <ayarimahdi287@gmail.com>
2025-04-11 23:29:02 +0100

    user Keyloak + Advanced features

--- backend/gateway/src/main/java/tn/esprit/gateway/GatewayApplication.java
+++ backend/gateway/src/main/java/tn/esprit/gateway/GatewayApplication.java
@@ -17,9 +17,19 @@ public class GatewayApplication {
 	@Bean
 	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
 		return builder.routes()
+<<<<<<< Updated upstream
 				.route("microservice1", r-> r.path("/candidat/**").uri("lb://microservice1")).
 				 route("microservice2", r-> r.path("/membre/**").uri("lb://microservice2")).
 				.route("microservice", r-> r.path("/competition/**").uri("lb://microservice")).
 				build();
+=======
+				.route("microservice1", r-> r.path("/api/salles/**").uri("lb://microservice1"))
+				.route("microservice5", r-> r.path("/abonnements/**").uri("lb://microservice5"))
+				.route("microservice2", r-> r.path("/stock/**").uri("lb://microservice2"))
+				.route("microservice", r-> r.path("/competition/**").uri("lb://microservice"))
+				.route("microservice4", r -> r.path("/plans/**").uri("lb://microservice4"))
+				.route("user-microservice", r -> r.path("/auth/**").uri("lb://user-microservice"))
+				.build();
+>>>>>>> Stashed changes
 	}
 }
